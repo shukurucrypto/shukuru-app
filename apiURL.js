77e8400acf5ba53cd0e6e32d6be6699db6a8c322
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-05-29 12:52:54 +0300

    Updated backend url

--- apiURL.js
+++ apiURL.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://7c6c-41-75-171-132.ngrok-free.app/app'
-export const SOCKET_SERVER = 'https://7c6c-41-75-171-132.ngrok-free.app'
+// export const API_URL = 'https://7c6c-41-75-171-132.ngrok-free.app/app'
+// export const SOCKET_SERVER = 'https://7c6c-41-75-171-132.ngrok-free.app'
 
-// export const API_URL = 'https://shukuru.up.railway.app/app'
-// export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
+export const API_URL = 'https://shukuru.up.railway.app/app'
+export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
