06e9ce26577d3993fb9715b43bb2d9a0c60f066f
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-06-10 18:31:12 +0300

    Change api url

--- apiURL.js
+++ apiURL.js
@@ -1,5 +1,11 @@
-export const API_URL = 'https://949c-41-75-179-245.ngrok-free.app/app'
-export const SOCKET_SERVER = 'https://949c-41-75-179-245.ngrok-free.app'
+// NGROK
+// export const API_URL = 'https://949c-41-75-179-245.ngrok-free.app/app'
+// export const SOCKET_SERVER = 'https://949c-41-75-179-245.ngrok-free.app'
 
+// TEST API
+export const API_URL = 'https://shukuru-test-api.up.railway.app/app'
+export const SOCKET_SERVER = 'https://shukuru-test-api.up.railway.app'
+
+// PRODUCTION
 // export const API_URL = 'https://shukuru.up.railway.app/app'
 // export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
