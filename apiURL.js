eb53d7a9d0d91a9f9593960b7f1ac63d4bce86fb
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-06-13 15:33:59 +0300

    Versioned the react native app

--- apiURL.js
+++ apiURL.js
@@ -3,9 +3,9 @@
 // export const SOCKET_SERVER = 'https://6a14-41-75-181-225.ngrok-free.app'
 
 // TEST API
-export const API_URL = 'https://shukuru-test-api.up.railway.app/app'
-export const SOCKET_SERVER = 'https://shukuru-test-api.up.railway.app'
+// export const API_URL = 'https://shukuru-test-api.up.railway.app/app'
+// export const SOCKET_SERVER = 'https://shukuru-test-api.up.railway.app'
 
 // PRODUCTION
-// export const API_URL = 'https://shukuru.up.railway.app/app'
-// export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
+export const API_URL = 'https://shukuru.up.railway.app/app'
+export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
