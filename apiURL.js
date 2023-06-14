296602848944866486d584a433a3d615708bc74b
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-06-14 17:47:26 +0300

    Added masa phone auth

--- apiURL.js
+++ apiURL.js
@@ -3,9 +3,9 @@
 // export const SOCKET_SERVER = 'https://900d-41-75-170-195.ngrok-free.app'
 
 // TEST API
-// export const API_URL = 'https://shukuru-test-api.up.railway.app/app'
-// export const SOCKET_SERVER = 'https://shukuru-test-api.up.railway.app'
+export const API_URL = 'https://shukuru-test-api.up.railway.app/app'
+export const SOCKET_SERVER = 'https://shukuru-test-api.up.railway.app'
 
 // PRODUCTION
-export const API_URL = 'https://shukuru.up.railway.app/app'
-export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
+// export const API_URL = 'https://shukuru.up.railway.app/app'
+// export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
