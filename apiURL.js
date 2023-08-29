7587e9c9249b1a26ee8f1bb81a939e49b89da7c4
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-08-29 09:26:06 +0300

    ngrok

--- apiURL.js
+++ apiURL.js
@@ -1,6 +1,6 @@
 // NGROK
-export const API_URL = 'https://5d13-41-75-172-189.ngrok-free.app/app'
-export const SOCKET_SERVER = 'https://5d13-41-75-172-189.ngrok-free.app'
+export const API_URL = 'https://aa95-41-75-171-139.ngrok-free.app/app'
+export const SOCKET_SERVER = 'https://aa95-41-75-171-139.ngrok-free.app'
 
 // TEST API
 // export const API_URL = 'https://shukuru-test-api.up.railway.app/app'
