d42fcda89a653589770a13e9e8109e09e125d140
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-09-21 14:04:18 +0300

    Added select network dialog

--- screens/Withdraw/EnterPhoneScreen.jsx
+++ screens/Withdraw/EnterPhoneScreen.jsx
@@ -1,11 +1,27 @@
-import { View, Text, SafeAreaView, TextInput, Pressable } from 'react-native'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  Pressable,
+  Image,
+} from 'react-native'
 import React, { useState } from 'react'
 import AppText from '../../components/AppText'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import BackHeader from '../../components/Headers/BackHeader'
+import SelectDropdown from 'react-native-select-dropdown'
+
+import mtn from '../../assets/logos/mtn.jpeg'
+import airtel from '../../assets/logos/airtel.jpeg'
+import ConfirmPhoneDialog from '../../components/Modals/ConfirmPhoneDialog'
 
 const EnterPhoneScreen = () => {
   const [mobileNetwork, setMobileNetwork] = useState('MTN')
+  const network = ['MTN', 'AIRTEL']
+
+  const [isModalVisible, setModalVisible] = useState(false)
+
   const [phone, setPhone] = useState(null)
 
   const navigation = useNavigation()
@@ -14,12 +30,63 @@ const EnterPhoneScreen = () => {
 
   const { data } = router.params
 
+  const handleSubmit = () => {
+    setModalVisible(false)
+
+    navigation.navigate('ReviewScreen', {
+      data,
+      phone,
+      mobileNetwork,
+    })
+  }
+
   return (
     <SafeAreaView className="flex flex-1">
       <BackHeader title="Enter Mobile" subTitle="Powered by OneRamp" cancel />
 
       <View className="flex-col flex-1 p-5">
-        <View className="flex flex-col ">
+        <AppText classProps="text-base mb-3">Select mobile network</AppText>
+        <View className="flex flex-row items-center ">
+          <SelectDropdown
+            data={network}
+            onSelect={(selectedItem, index) => {
+              setMobileNetwork(selectedItem) // console.log(selectedItem, index)
+            }}
+            rowTextStyle={{
+              fontSize: 15,
+            }}
+            buttonTextStyle={{
+              fontSize: 16,
+            }}
+            defaultValue={'MTN'}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // text represented after item is selected
+              // if data array is an array of objects then return selectedItem.property to render after item is selected
+              return selectedItem
+            }}
+            rowTextForSelection={(item, index) => {
+              // text represented for each item in dropdown
+              // if data array is an array of objects then return item.property to represent item in dropdown
+              return item
+            }}
+            buttonStyle={{
+              margin: 0,
+              backgroundColor: '#fff',
+              width: 110,
+              borderRadius: 8,
+              backgroundColor: '#fafafa',
+            }}
+          />
+
+          <View className="relative w-12 h-12 mx-4 overflow-hidden rounded-md bg-neutral-50">
+            <Image
+              source={mobileNetwork === 'MTN' ? mtn : airtel}
+              style={{ width: '100%', height: '100%' }}
+              resizeMode="cover"
+            />
+          </View>
+        </View>
+        {/* <View className="flex flex-col ">
           <AppText classProps="text-lg">Mobile Network</AppText>
           <TextInput
             defaultValue={mobileNetwork}
@@ -27,17 +94,19 @@ const EnterPhoneScreen = () => {
             onChangeText={(e) => setMobileNetwork(e)}
             className="px-3 my-2 text-black border rounded-md h-14 border-neutral-400"
           />
-        </View>
+        </View> */}
 
         {/*  */}
-        <View className="flex flex-col mt-5 ">
-          <AppText classProps="text-lg">Phone Number</AppText>
+        <View className="flex flex-col mt-8 ">
+          <AppText classProps="text-base">Enter phone number</AppText>
           <TextInput
             placeholder="+256XXXXXXXX"
             keyboardType="phone-pad"
             value={phone}
             onChangeText={(e) => setPhone(e)}
-            className="p-3 my-2 text-black border rounded-md h-14 border-neutral-400"
+            returnKeyType="done"
+            onSubmitEditing={() => setModalVisible(true)}
+            className="p-3 my-2 text-black border rounded-md h-14 border-neutral-300"
           />
         </View>
       </View>
@@ -45,13 +114,7 @@ const EnterPhoneScreen = () => {
       <View className="p-5">
         {/* Button */}
         <Pressable
-          onPress={() =>
-            navigation.navigate('ReviewScreen', {
-              data,
-              phone,
-              mobileNetwork,
-            })
-          }
+          onPress={() => setModalVisible(true)}
           disabled={!phone}
           className={`items-center self-center  py-3 rounded-full w-full ${
             phone ? 'bg-primary' : 'bg-neutral-200'
@@ -66,6 +129,12 @@ const EnterPhoneScreen = () => {
           </Text>
         </Pressable>
       </View>
+      <ConfirmPhoneDialog
+        isModalVisible={isModalVisible}
+        setModalVisible={setModalVisible}
+        phoneNumber={phone}
+        handleSubmit={handleSubmit}
+      />
     </SafeAreaView>
   )
 }
