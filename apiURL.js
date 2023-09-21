9664acc25dfdd16277a632bb549325910a1059db
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-09-21 13:18:41 +0300

    Put to staging

--- apiURL.js
+++ apiURL.js
@@ -1,6 +1,6 @@
 // NGROK
-export const API_URL = 'https://93c5-41-210-146-209.ngrok-free.app/app'
-export const SOCKET_SERVER = 'https://93c5-41-210-146-209.ngrok-free.app'
+export const API_URL = 'https://shukuru-api-test.up.railway.app/app'
+export const SOCKET_SERVER = 'https://shukuru-api-test.up.railway.app/'
 
 // TEST API
 // export const API_URL = 'https://shukuru-test-api.up.railway.app/app'
