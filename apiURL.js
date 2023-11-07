b10c299cc85d872942f6dcd4de18835421f45b89
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-11-07 16:48:58 +0300

    Add forgot password and reset functionality

--- apiURL.js
+++ apiURL.js
@@ -3,9 +3,9 @@
 // export const SOCKET_SERVER = 'https://aa03-41-75-179-106.ngrok-free.app'
 
 // TEST API
-export const API_URL = 'https://shukuru-api-test.up.railway.app/app'
-export const SOCKET_SERVER = 'https://shukuru-api-test.up.railway.app/'
+// export const API_URL = 'https://shukuru-api-test.up.railway.app/app'
+// export const SOCKET_SERVER = 'https://shukuru-api-test.up.railway.app/'
 
 // PRODUCTION
-// export const API_URL = 'https://shukuru.up.railway.app/app'
-// export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
+export const API_URL = 'https://shukuru.up.railway.app/app'
+export const SOCKET_SERVER = 'https://shukuru.up.railway.app'
